1db2f4bbbac8a7553ca5b03eef8a4bdf58f0213a
Georgina Kidger <georgina.kidger@hotmail.com>
2025-03-22 22:36:55 -0500

    finished product size logic and vat_total

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,25 +10,31 @@ def bag_contents(request):
     """
     bag_items = []
     total = 0
+    subtotal = 0
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
-
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PRICE / 100)
-        free_delivery_calc = settings.FREE_DELIVERY_THRESHOLD - total
-    else:
-        delivery = 0
-        free_delivery_calc = 0
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for shoesize, quantity in item_data['items_by_shoesize'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'shoesize': shoesize,
+                })          
 
     if request.path in ['/bag/', '/checkout/']:
         vat_calc = Decimal('0.20')  # calculates the 20% UK VAT
@@ -36,12 +42,23 @@ def bag_contents(request):
     else:
         vat_total = Decimal('0.00')
 
+    subtotal = total + vat_total
+
+    if subtotal < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PRICE / 100)
+        free_delivery_calc = settings.FREE_DELIVERY_THRESHOLD - subtotal
+    else:
+        delivery = 0
+        free_delivery_calc = 0
+
     grand_total = delivery + total + vat_total
 
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
+        'subtotal': subtotal,
+        'vat_total': vat_total,
         'delivery': delivery,
         'free_delivery_calc': free_delivery_calc,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
--- bag/views.py
+++ bag/views.py
@@ -15,12 +15,24 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    shoesize = None
+    if 'product_size' in request.POST:
+        shoesize = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if shoesize:
+        if item_id in list(bag.keys()):
+            if shoesize in bag[item_id]['items_by_shoesize'].keys():
+                bag[item_id]['items_by_shoesize'][shoesize] += quantity
+            else:
+                bag[item_id]['items_by_shoesize'][shoesize] = quantity
+        else:
+            bag[item_id] = {'items_by_shoesize': {shoesize: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
