3ad4686757db94f21cb9e12cfa57378e2fb88bbc
Georgina Kidger <georgina.kidger@hotmail.com>
2025-03-22 21:01:40 -0500

    added contexts for add to bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
@@ -9,15 +11,32 @@ def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PRICE / 100)
         free_delivery_calc = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_calc = 0
 
-    grand_total = delivery + total
+    if request.path in ['/bag/', '/checkout/']:
+        vat_calc = Decimal('0.20')  # calculates the 20% UK VAT
+        vat_total = (total * vat_calc).quantize(Decimal('0.01'))
+    else:
+        vat_total = Decimal('0.00')
+
+    grand_total = delivery + total + vat_total
 
     context = {
         'bag_items': bag_items,
