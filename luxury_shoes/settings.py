ab96bb099a6dd48f99a491fe0fda14a1a719e448
Georgina Kidger <georgina.kidger@hotmail.com>
2025-03-24 20:00:05 -0500

    added checkout template with crispy forms

--- luxury_shoes/settings.py
+++ luxury_shoes/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
 
+    # external
+    'crispy_forms',
+    'django_countries',
+
 ]
 
 MIDDLEWARE = [
@@ -50,6 +54,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'luxury_shoes.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -66,8 +72,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
