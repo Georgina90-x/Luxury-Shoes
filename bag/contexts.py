3948a54e5626e805bcfccd14d3c2fd3e640f4ee0
Georgina Kidger <georgina.kidger@hotmail.com>
2025-03-27 21:56:42 -0500

    added order history and updated checkout view to prefill form

--- bag/contexts.py
+++ bag/contexts.py
@@ -43,7 +43,7 @@ def bag_contents(request):
 
     if subtotal < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PRICE / 100)
-        free_delivery_calc = settings.FREE_DELIVERY_THRESHOLD - (total + vat_total)
+        free_delivery_calc = settings.FREE_DELIVERY_THRESHOLD - subtotal
     else:
         delivery = 0
         free_delivery_calc = 0
