327dfdf4275f88d03df55a265e1796a05aaa3284
Georgina Kidger <georgina.kidger@hotmail.com>
2025-03-29 17:51:14 -0500

    testing if cache is preventing checkout on heroku

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -61,9 +61,10 @@ form.addEventListener('submit', function(ev) {
         'client_secret': clientSecret,
         'save_info': saveInfo,
     };
-    var url = '/checkout/cache_checkout_data/';
-
+    //var url = '/checkout/cache_checkout_data/';
+/*
     $.post(url, postData).done(function(){
+    */
         stripe.confirmCardPayment(clientSecret, {
             payment_method: {
                 card: card,
