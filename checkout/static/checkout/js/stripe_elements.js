bef1656ba81e5f95eff44cd1f8d2b560cc23a932
Georgina Kidger <georgina.kidger@hotmail.com>
2025-03-29 18:39:41 -0500

    amended typo in settings and reverted javascript

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -107,7 +107,7 @@ form.addEventListener('submit', function(ev) {
                 $('#submit-button').attr('disabled', false);
             } else {
                 if (result.paymentIntent.status === 'succeeded') {
-                    window.location.href = "/checkout/success/";
+                    form.submit();
                 }
             }
         });
--- luxury_shoes/settings.py
+++ luxury_shoes/settings.py
@@ -120,7 +120,7 @@ WSGI_APPLICATION = 'luxury_shoes.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if 'DATABSE_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default':
         dj_database_url.parse(os.environ.get('DATABASE_URL'))
