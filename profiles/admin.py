cc3ef4b5d57449aa28339d16c51eb03780519156
Georgina Kidger <georgina.kidger@hotmail.com>
2025-03-28 23:24:06 -0500

    added admin view and amended email template

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import MarketingEmail, EmailLog
 
-# Register your models here.
+
+@admin.register(MarketingEmail)
+class MarketingEmailAdmin(admin.ModelAdmin):
+    list_display = ("subject", "sent_at")  # Display subject & timestamp
+    search_fields = ("subject",)  # Add search functionality
+    ordering = ("-sent_at",)  # Sort by most recent
+
+@admin.register(EmailLog)
+class EmailLogAdmin(admin.ModelAdmin):
+    list_display = ("user", "email", "sent_at")  # Display user, email & timestamp
+    search_fields = ("user__email", "email__subject")  # Search by user email & subject
+    list_filter = ("sent_at",)  # Add filter by date
