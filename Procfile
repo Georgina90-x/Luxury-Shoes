d1665dc797865dd1bd2daf13bda7b5fb64099a3c
Georgina Kidger <georgina.kidger@hotmail.com>
2025-03-29 15:46:58 -0500

    prepared project for heroku deployment

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn
+luxury_shoes.wsgi:application
--- luxury_shoes/settings.py
+++ luxury_shoes/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -14,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h^zf5gw45ygg!8)0ii&(w5md1+9db+%y7g5m-0d87996o4ihc4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -22,6 +23,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',  # VS Code Preview
     'localhost',  # Stripe webhook listener
+    'luxury-shoes-00974b0f1528.herokuapp.com/'  # heroku application
 ]
 
 
@@ -117,13 +119,18 @@ WSGI_APPLICATION = 'luxury_shoes.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABSE_URL' in os.environ:
+    DATABASES = {
+        'default':
+        dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
