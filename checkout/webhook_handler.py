acdc33e6dc98be75a601864913ebdcba9c112b40
Georgina Kidger <georgina.kidger@hotmail.com>
2025-03-30 16:48:45 -0500

    amended wh secret key and webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -157,8 +157,7 @@ class StripeWH_Handler:
         self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: \
-                  Created order in webhook.'
-        )
+                  Created order in webhook.', status=200)
 
     def handle_payment_intent_failed(self, event):
         """
